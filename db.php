192203ffe9cd822b017d198242db9b24e6d9b6c8
Ganesh369 <dilliganesh@blackitechs.com>
2025-01-07 15:18:27 +0530

    changes to connect  local DB

--- db.php
+++ db.php
@@ -4,20 +4,29 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-$dbHost = '217.21.88.10';
-$dbPort = '3306';
-$dbUsername = 'u140987190_choozhal_dev';
-$dbPassword = 'Choozhal_dev@7';
+// $dbHost = '217.21.88.10';
+// $dbPort = '3306';
+// $dbUsername = 'u140987190_choozhal_dev';
+// $dbPassword = 'Choozhal_dev@7';
+// $dbName = 'u140987190_choozhal_app';
+
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
 $dbName = 'u140987190_choozhal_app';
 
 // Connect to the database
-$conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName, $dbPort);
+// $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName, $dbPort);
+$conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
 
 if (!$conn) {
   die('Connection failed: ' . mysqli_connect_error());
 }
 try {
-  $pdo = new PDO('mysql:host=' . $dbHost . ';port=' . $dbPort . ';dbname=' . $dbName, $dbUsername, $dbPassword);
+  // $pdo = new PDO('mysql:host=' . $dbHost . ';port=' . $dbPort . ';dbname=' . $dbName, $dbUsername, $dbPassword);
+  $pdo = new PDO('mysql:host=' . $dbHost . ';dbname=' . $dbName, $dbUsername, $dbPassword);
+
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   die("ERROR: Could not connect. " . $e->getMessage());
--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-// header("Content-Type: application/json");
+require_once "db.php";
 
-$dbHost = '217.21.88.10';
-$dbPort = '3306';
-$dbUsername = 'u140987190_choozhal_dev';
-$dbPassword = 'Choozhal_dev@7';
-$dbName = 'u140987190_choozhal_app';
 
-$conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName, $dbPort);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
